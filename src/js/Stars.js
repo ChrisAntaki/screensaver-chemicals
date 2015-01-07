cee3f4ce01b0cf736e9f9dc30d8f09cadab0b828
Chris Antaki <ChrisAntaki@gmail.com>
2015-01-07 01:44:21 -0800

    Dynamically adjusting intensity, based on FPS

--- src/js/Stars.js
+++ src/js/Stars.js
@@ -3,6 +3,10 @@ var TimeQueue = require('./time-queue');
 var Star = require('./Star');
 
 function Stars() {
+    this.lastFrames = [];
+
+    this.createStarDelay = 128;
+
     this.svg = document.querySelector('svg');
 
     this.queue = new TimeQueue();
@@ -16,6 +20,7 @@ function Stars() {
     this.requestAnimationFrame = this.requestAnimationFrame.bind(this);
     this.requestAnimationFrame();
 
+    this.queueDisplayFPS();
     this.queueCreateStar();
 }
 
@@ -23,14 +28,23 @@ Stars.prototype.queueCreateStar = function() {
     this.queue.set({
         callback: this.createStar,
         context: this,
-        time: Date.now() + 16,
+        time: Date.now() + this.createStarDelay,
     });
 };
 
 Stars.prototype.requestAnimationFrame = function() {
     requestAnimationFrame(this.requestAnimationFrame);
     this.queue.tick();
+    this.calculateFPS();
 };
+
+Stars.prototype.calculateFPS = function() {
+    if (this.lastFrames.length > 60) {
+        this.lastFrames.shift();
+    }
+
+    this.lastFrames.push(Date.now());
+}
 
 Stars.prototype.createStar = function() {
     var star = new Star({
@@ -45,6 +59,33 @@ Stars.prototype.createStar = function() {
     });
 
     this.queueCreateStar();
+};
+
+Stars.prototype.queueDisplayFPS = function() {
+    this.queue.set({
+        callback: this.displayFPS,
+        context: this,
+        time: Date.now() + 1000,
+    });
+};
+
+Stars.prototype.displayFPS = function() {
+    var length = this.lastFrames.length;
+    var difference = (this.lastFrames[length - 1] - this.lastFrames[0]) / (length - 1);
+    var fps = 1000 / difference;
+    console.log(fps);
+
+    if (fps < 55 && this.createStarDelay < 1000) {
+        this.createStarDelay += 10;
+        // console.log('Added 10ms to this.createStarDelay');
+        // console.log('this.createStarDelay:', this.createStarDelay);
+    } else if (fps > 59 && this.createStarDelay > 16) {
+        this.createStarDelay -= 1;
+        // console.log('Removed 1ms from this.createStarDelay');
+        // console.log('this.createStarDelay:', this.createStarDelay);
+    }
+
+    this.queueDisplayFPS();
 };
 
 function rand(maximum) {
